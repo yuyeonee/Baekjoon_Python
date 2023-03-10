0aad21b685e44c04420fc501063a04519e98795f
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-03-11 02:08:56 +0900

    피보나치 수 4
    3/11

--- 10826.py
+++ 10826.py
@@ -3,12 +3,10 @@ n = int(input())
 num = [0]*(n+1)
 
 num[0] = 0
-num[1] = 1
+if n>0:
+    num[1] = 1
 
 for i in range(1,n):
     num[i+1] = num[i] + num[i-1]
 
-if n>1:
-    print(num[n])
-else:
-    print(n)
+print(num[n])
