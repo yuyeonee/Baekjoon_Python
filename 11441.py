e24c502337f3891f6a105c4b0b35728db1a26d46
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-04-08 21:46:52 +0900

    합 구하기
    4/8
    구간 합 구하기4랑 똑같음 구간 합 미리 구하기

---
+++ 11441.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+nums = list(map(int, input().split()))
+m = int(input())
+
+dp = nums
+for i in range(1, n):
+    dp[i] = dp[i] + dp[i-1]
+dp.insert(0, 0)
+
+for i in range(m):
+    left, right = map(int, sys.stdin.readline().split())
+    print(dp[right] - dp[left-1])
