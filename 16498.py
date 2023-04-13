9e9c6d57fa643e93fb04c868247c2a577b0b4808
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-04-14 03:31:24 +0900

    작은 벌점 - 다시
    4/13

---
+++ 16498.py
@@ -0,0 +1,15 @@
+from itertools import product
+
+a, b, c = map(int, input().split())
+nums = []
+for i in range(3):
+    nums.append(set(map(int, input().split())))
+
+combi = set(product(*nums))
+ans = []
+
+for cb in combi:
+    d = max(cb) - min(cb)
+    ans.append(d)
+print(min(ans))
+
