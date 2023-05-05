2f5e4e3a24bd7b2d56ef0de857c3d2697c1fa5e2
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-05-05 19:02:29 +0900

    대지

---
+++ 9063.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+x = []
+y = []
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    x.append(a)
+    y.append(b)
+
+print((max(x)-min(x))*(max(y)-min(y)))
