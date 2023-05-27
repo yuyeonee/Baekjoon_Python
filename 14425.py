78b8d667c4e96b69e717e8f50aa3221adae35498
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-05-27 23:56:20 +0900

    문자열 집합

--- 14425.py
+++ 14425.py
@@ -2,7 +2,7 @@ import sys
 
 n, m = map(int, input().split())
 
-S = set(sys.stdin.readline().strip() for i in range(n))
+S = set(sys.stdin.readline().strip() for i in range(n)) # list로 했을 때 시간 초과
 T = list(sys.stdin.readline().strip() for i in range(m))
 
 count = 0
