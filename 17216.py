e798aee7a81ca5fd20b95e7c46744e4495245ef8
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-06-14 19:07:34 +0900

    가장 큰 감소 부분 수열
    dp

---
+++ 17216.py
@@ -0,0 +1,11 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = nums[:]
+
+for i in range(1, n):
+    for j in range(i):
+        if nums[i]<nums[j]:
+            dp[i] = max(dp[i], dp[j]+nums[i])
+            
+print(max(dp))
