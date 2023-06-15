25961e46fb52a06bebdc29dc4acb184765914927
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-06-16 00:09:20 +0900

    가장 긴 증가하는 부분 수열 2
    dp 
    bisect

---
+++ 12015.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+dp = [] 
+
+for num in nums:
+    ind = bisect_left(dp, num)
+    if ind==len(dp):
+        dp.append(num)
+    else:
+        dp[ind] = num
+        
+print(len(dp))
