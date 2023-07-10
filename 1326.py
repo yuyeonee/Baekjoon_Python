25876b3b2673ba1e0767bfcf70e25b2c01a3a8ac
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2023-07-10 19:30:49 +0900

    폴짝폴짝
    bfs
    for문 잘 써서 양쪽 탐색

--- 1326.py
+++ 1326.py
@@ -1,14 +1,34 @@
 from collections import deque
 
 n = int(input())
-nums = list(map(int, input().split()))
+jumps = [0] + list(map(int, input().split()))
 a, b = map(int, input().split())
 
-queue = deque() 
-queue.append(a-1)
+def bfs(a, b): 
+    queue = deque([a]) 
+    visited = [-1]*(n+1)
+    visited[a] = 0
 
-while queue:
-    now = queue.popleft()
-    
-    for jump in 
+    while queue:
+        now = queue.popleft()
+        
+        for i in range(now, n+1, jumps[now]):
+            if visited[i]==-1:
+                visited[i] = visited[now] + 1
+                queue.append(i)
+                if i==b:
+                    return visited[i]
+        for i in range(now, 0, -jumps[now]):
+            if visited[i]==-1:
+                visited[i] = visited[now] + 1
+                queue.append(i)
+                if i==b:
+                    return visited[i]
+    return -1
+
+print(bfs(a,b))
+
+
+
+
 
