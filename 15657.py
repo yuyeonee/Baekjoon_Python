007aca24ceada78ddb58eeb4c3c42a6b5ffcdba8
Yuyeon <63159699+yuyeonee@users.noreply.github.com>
2023-09-30 00:21:21 +0900

    N과 M (8)
    dfs
    backtracking

---
+++ 15657.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+def dfs(m, ans):
+    if M==m:
+        print(*ans)
+        return
+    else:
+        if m==0:
+            for i in range(N):
+                dfs(m+1, ans+[nums[i]])
+        else:
+            for i in range(N):
+                if ans[-1]<=nums[i]:
+                    dfs(m+1, ans+[nums[i]])
+
+ans = []
+dfs(0, ans)
+    
